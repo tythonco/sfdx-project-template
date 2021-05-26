4c536e37092b448b50d97c7d1dce86cd22992ad7
Scott Covert <scott.b.covert@gmail.com>
2021-05-25 22:31:58 -0400

    Replaced CircleCI with GitHub Actions

--- scripts/ci-setup-prod.sh
+++
@@ -1,7 +0,0 @@
-# Get the private url from environment variable, create required file for cmd
-echo "Setting up Prod Connection..."
-openssl enc -d -aes-256-cbc -md md5 -in prod_auth_url.txt.enc -out prod_auth_url.txt -k $AUTH_FILE_KEY
-
-# Authenticate to salesforce Prod org
-echo "Authenticating..."
-sfdx force:auth:sfdxurl:store -f prod_auth_url.txt -a Prod && rm prod_auth_url.txt

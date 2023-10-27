c73870b8a7452f4a2a1f07c4a5f97e627d0eb6e9
Charles Ross <chivalry@mac.com>
2023-10-27 15:57:07 -0700

    Update auth decrypt

--- scripts/setup-auth-decrypt.sh
+++ scripts/setup-auth-decrypt.sh
@@ -10,7 +10,7 @@ QA_AUTH_FILE=qa_auth_url.txt
 
 if [ -z "$PROJECT_NAME" ]
 then
-      read -p "Enter project name " PROJECT_NAME
+    read -p "Enter project name " PROJECT_NAME
 fi
 
 echo ""
@@ -29,7 +29,11 @@ while true; do
     read -p "Is the production org the same as the DevHub org? (This is the case in most instances except for ISV projects) " DEVHUB_IS_PROD
     case $DEVHUB_IS_PROD in
         [Yy]* ) break;;
-        [Nn]* ) echo "" && echo "Authenticating to Prod org..." && sfdx force:auth:sfdxurl:store -f ${PROD_AUTH_FILE} -a ${PROD_NAME} && break;;
+        [Nn]* ) echo "" && echo "Authenticating to Prod org..." && \
+            sf auth sfdxurl store \
+                --sfdx-auth-file ${PROD_AUTH_FILE} \
+                --alias ${PROD_NAME} \
+            && break;;
         * ) echo "y/n.";;
     esac
 done
@@ -38,7 +42,9 @@ done
 if [ -f "${QA_AUTH_FILE}.enc" ]; then
     echo "" && echo "Authenticating to QA org"
     if openssl enc -d -aes-256-cbc -md md5 -in ${QA_AUTH_FILE}.enc -out ${QA_AUTH_FILE} -k ${AUTH_URL_PASSWORD} ; then
-        sfdx force:auth:sfdxurl:store -f ${QA_AUTH_FILE} -a ${QA_NAME}
+        sf auth sfdxurl store \
+            --sfdx-auth-file ${QA_AUTH_FILE} \
+            --alias ${QA_NAME} \
     else
         echo "" && echo "ERROR: Problem decrypting auth file, please double-check your password and verify ${QA_AUTH_FILE}.enc exists in the source"
         exit
@@ -48,7 +54,9 @@ fi
 # Decrypt DevHub auth file & set up an alias for the DevHub org
 echo "" && echo "Authenticating to DevHub org..."
 if openssl enc -d -aes-256-cbc -md md5 -in ${AUTH_FILE}.enc -out ${AUTH_FILE} -k ${AUTH_URL_PASSWORD} ; then
-    sfdx force:auth:sfdxurl:store -f ${AUTH_FILE} -d -a ${DEVHUB_NAME}
+    sf auth sfdxurl store \
+        --sfdx-auth-file ${AUTH_FILE} \
+        --alias ${DEVHUB_NAME} \
     echo "" && echo "Initial setup completed!" && echo "Running 'npm start' should now work to create new scratch orgs."
 else
     echo "" && echo "ERROR: Problem decrypting auth file, please double-check your password and verify ${AUTH_FILE}.enc exists in the source"

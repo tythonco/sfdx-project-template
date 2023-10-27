dd30be7209bb5f0ef4cae05ee3888d74b0b73276
Charles Ross <chivalry@mac.com>
2023-10-27 15:28:08 -0700

    Update install test package

--- scripts/install-test-package.sh
+++ scripts/install-test-package.sh
@@ -14,23 +14,32 @@ TEST_ORG="${PROJECT_NAME}PackageTestOrg"
 
 
 if [ -z "$PACKAGE_VER_ID" ]; then
-    PACKAGE_VER_ID=$(grep "${PROJECT_NAME}" sfdx-project.json | tail -1 | sed -E 's/^.*"(04t[[:alnum:]]*)"$/\1/')
+    PACKAGE_VER_ID=$(grep "${PROJECT_NAME}" sfdx-project.json \
+        | tail -1 \
+        | sed -E 's/^.*"(04t[[:alnum:]]*)"$/\1/')
 fi
 
 echo "Install package to temporary scratch org for testing with version ID: ${PACKAGE_VER_ID} ... "
 
 # Check if "PackageTestOrg" already exists, delete if it does
-if sfdx force:org:list | grep "$TEST_ORG"; then
+if sf org list | grep "$TEST_ORG"; then
     echo "Pre-existing test scratch org detected! Deleting ..."
-    sfdx force:org:delete -u "$TEST_ORG" -p
+    sf org delete scratch \
+        --target-org "$TEST_ORG" \
+        --no-prompt
 fi
 
 # Generate a fresh scratch org to install & test the package
 # Ensure namespace is NOT applied to this org since this is to simulate a customer install
-sfdx force:org:create --nonamespace --definitionfile config/project-scratch-def.json --setalias "$TEST_ORG"
+sf org create scratch \
+    --no-namespace \
+    --definition-file config/project-scratch-def.json \
+    --alias "$TEST_ORG"
 
 # Install the package
-sfdx force:package:install --package "$PACKAGE_VER_ID" --targetusername "$TEST_ORG"
+sf package install \
+    --package "$PACKAGE_VER_ID" \
+    --target-org "$TEST_ORG"
 
 unset PACKAGE_VER_ID
 
@@ -72,6 +81,6 @@ echo ""
 echo "Opening scratch org for testing, may the Flow be with you!"
 echo ""
 sleep 3
-sfdx force:org:open --targetusername "$TEST_ORG"
+sf org open --target-org "$TEST_ORG"
 
 exit
